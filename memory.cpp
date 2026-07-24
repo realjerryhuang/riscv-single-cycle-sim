8a37ba550a0f964cfb9819e5c0078abab73067c1
Jerry Huang <huangjerry67@gmail.com>
2026-07-23 19:36:39 -0700

    Implemented constructor, write half word, write word, read byte, and read word functions for data memory.

--- memory.cpp
+++ memory.cpp
@@ -5,7 +5,7 @@ InstructionMemory::InstructionMemory() : instMem{}
 {
 }
 
-/* Given an address and instruction, store the instruction at that address. */
+/* Given an address and instruction, store the instruction at that address in little-endian format. */
 void InstructionMemory::writeInst(uint32_t addr, uint32_t inst)
 {
     uint8_t byte3 = (inst >> 24) & 0xFF;    // Right shift 24 bits to align top 8 bits to the right, then mask to isolate those 8 bits
@@ -19,7 +19,7 @@ void InstructionMemory::writeInst(uint32_t addr, uint32_t inst)
     instMem[addr+3] = byte3;
 }
 
-/* Given an address, return the instruction stored in the instruction memory at that address. */
+/* Given an address, return the instruction stored in the instruction memory at that address in big-endian format. */
 uint32_t InstructionMemory::readInst(uint32_t addr) const
 {
     // Return 4 bytes of instMem array starting at the index given by addr
@@ -27,6 +27,55 @@ uint32_t InstructionMemory::readInst(uint32_t addr) const
     uint8_t byte1 = instMem[addr+1];
     uint8_t byte2 = instMem[addr+2];
     uint8_t byte3 = instMem[addr+3];
+
+    return (static_cast<uint32_t>(byte3) << 24) | 
+        (static_cast<uint32_t>(byte2) << 16) | 
+        (static_cast<uint32_t>(byte1) << 8) | 
+        (static_cast<uint32_t>(byte0));
+}
+
+/* Instantiate data memory by filling cells with all zeros. */
+DataMemory::DataMemory() : dataMem{}
+{
+}
+
+/* Given an address and 16-bit data, store the data at that address. */
+void DataMemory::writeHalf(uint32_t addr, uint16_t half)        // addr should be a multiple of 2
+{
+    uint8_t byte1 = (half >> 8) & 0xFF;
+    uint8_t byte0 = half & 0xFF;
+
+    dataMem[addr] = byte0;
+    dataMem[addr+1] = byte1;
+}
+
+/* Given an address and 32-bit data, store the data at that address. */
+void DataMemory::writeWord(uint32_t addr, uint32_t word)        // addr should be a multiple of 4
+{
+    uint8_t byte3 = (word >> 24) & 0xFF;
+    uint8_t byte2 = (word >> 16) & 0xFF;
+    uint8_t byte1 = (word >> 8) & 0xFF;
+    uint8_t byte0 = word & 0xFF;
+
+    dataMem[addr] = byte0;
+    dataMem[addr+1] = byte1;
+    dataMem[addr+2] = byte2;
+    dataMem[addr+3] = byte3;
+}
+
+/* Given an address, return the 8-bit data stored in the data memory at that address. */
+uint8_t DataMemory::readByte(uint32_t addr) const               // addr should be a multiple of 1
+{
+    return dataMem[addr];
+}
+
+/* Given an address, return the 32-bit data stored in the data memory at that address. */
+uint32_t DataMemory::readWord(uint32_t addr) const              // addr should be a multiple of 4
+{
+    uint8_t byte0 = dataMem[addr];
+    uint8_t byte1 = dataMem[addr+1];
+    uint8_t byte2 = dataMem[addr+2];
+    uint8_t byte3 = dataMem[addr+3];
 
     return (static_cast<uint32_t>(byte3) << 24) | 
         (static_cast<uint32_t>(byte2) << 16) | 
--- memory.h
+++ memory.h
@@ -10,10 +10,10 @@ class InstructionMemory
 {
     public:
         InstructionMemory();
-        void writeInst(uint32_t addr, uint32_t inst);
+        void writeInst(uint32_t addr, uint32_t inst);   // Address is 32 bits because RISC-V PC (RV32I) is 32 bits
         uint32_t readInst(uint32_t addr) const;
     private:
-        /* Byte-addressable instruction memory, addressable in little-endian fashion */
+        /* Byte-addressable instruction memory, addressable in little-endian fashion. */
         uint8_t instMem[INST_MEM_SIZE];    // Memory holds 4096 cells, each storing 1 byte (8 bits)
 };
 
@@ -21,9 +21,13 @@ class DataMemory
 {
     public:
         DataMemory();
+        void writeHalf(uint32_t addr, uint16_t half);       // Supports SH instruction
+        void writeWord(uint32_t addr, uint32_t word);       // Supports SW instruction
+        uint8_t readByte(uint32_t addr) const;              // Supports LBU instruction
+        uint32_t readWord(uint32_t addr) const;             // Supports LW instruction
     private:
-        int dataMem[DATA_MEM_SIZE];
-
+        /* Byte-addressable data memory, addressable in little-endian fashion. */
+        uint8_t dataMem[DATA_MEM_SIZE];
 };
 
 #endif  // MEMORY_INCLUDED
