fe403e3e7b757a33e1fbd7c308e8b6a6e2c0af48
Jerry Huang <huangjerry67@gmail.com>
2026-08-16 17:05:25 -0700

    Added shiftLeft1.h and implemented left-shift module right before branch ALU as a function.

---
+++ shiftLeft1.h
@@ -0,0 +1,11 @@
+#ifndef SHIFTLEFT1_INCLUDED
+#define SHIFTLEFT1_INCLUDED
+
+#include <cstdint>
+
+inline int32_t shiftLeft1(int32_t imm) 
+{
+	return static_cast<int32_t>(static_cast<uint32_t>(imm) << 1);	// Type-casting due to compiler shenanigans...
+}
+
+#endif SHIFTLEFT1_INCLUDED
